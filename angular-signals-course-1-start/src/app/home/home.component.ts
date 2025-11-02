e7ae9e0e8c2f72757aec86a44d416b2bd81a7698
guiziis <guigamoda@hotmail.com>
2025-11-02 16:00:19 -0300

    test with injector

--- angular-signals-course-1-start/src/app/home/home.component.ts
+++ angular-signals-course-1-start/src/app/home/home.component.ts
@@ -1,40 +1,49 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { Component, computed, effect, inject, Injector, OnInit, signal } from '@angular/core';
+import { CoursesService } from "../services/courses.service";
+import { Course, sortCoursesBySeqNo } from "../models/course.model";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { CoursesCardListComponent } from "../courses-card-list/courses-card-list.component";
+import { MatDialog } from "@angular/material/dialog";
+import { MessagesService } from "../messages/messages.service";
+import { catchError, from, throwError } from "rxjs";
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from "@angular/core/rxjs-interop";
 
 type Counter = {
   count: number
 };
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [
+    MatTabGroup,
+    MatTab,
+    CoursesCardListComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  counter = signal<Counter>({count: 0 });
-  values = signal<number[]>([1,2,3]);
+export class HomeComponent implements OnInit {
+  counter = signal<Counter>({ count: 0 });
+  values = signal<number[]>([1, 2, 3]);
+  injector = inject(Injector);
 
   tenXCounter = computed(() => {
     const current = this.counter();
-    return current.count *10;
+    return current.count * 10;
   });
 
   hundredXCounter = computed(() => {
     const current = this.tenXCounter();
     return current * 100;
   });
+
+  ngOnInit(): void {
+    effect(() => {
+      console.log('Counter changed:', this.counter());
+    }, {
+      injector: this.injector
+    });
+  }
 
   append(): void {
     this.values.update(vs => [...vs, vs.length + 1])
