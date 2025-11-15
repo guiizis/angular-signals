d57bf403436a4598606fabacd91a7f8f70230e87
guiziis <guigamoda@hotmail.com>
2025-11-15 15:03:05 -0300

    feat add onPush

--- lab-signals-main/src/app/effect/effect.component.ts
+++ lab-signals-main/src/app/effect/effect.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ElementService } from '../element.service';
 
 @Component({
   selector: 'app-effect',
   templateUrl: './effect.component.html',
-  styleUrl: './effect.component.css'
+  styleUrl: './effect.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EffectComponent {
   service: ElementService = inject(ElementService);
--- lab-signals-main/src/app/element-details/element-details.component.ts
+++ lab-signals-main/src/app/element-details/element-details.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ElementService } from '../element.service';
 
 @Component({
   selector: 'app-element-details',
   templateUrl: './element-details.component.html',
-  styleUrl: './element-details.component.css'
+  styleUrl: './element-details.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ElementDetailsComponent {
    elementService = inject(ElementService);
