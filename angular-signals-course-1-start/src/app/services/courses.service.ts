d31e5d1106641798b46237fb99d46311078fb274
guiziis <guigamoda@hotmail.com>
2025-11-30 15:24:23 -0300

    add context to request

--- angular-signals-course-1-start/src/app/services/courses.service.ts
+++ angular-signals-course-1-start/src/app/services/courses.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpContext } from "@angular/common/http";
 import { environment } from "../../environments/environment";
 import { firstValueFrom } from "rxjs";
 import { Course } from "../models/course.model";
 import { GetCoursesResponse } from "../models/get-courses.response";
+import { SkipLoadingContext } from "../skip-loading/skip-loading.context";
 
 
 @Injectable({
@@ -13,7 +14,9 @@ export class CoursesService {
   private readonly httpClient = inject(HttpClient);
 
   async loadAllCourses(): Promise<Course[]> {
-    const courses$ = this.httpClient.get<GetCoursesResponse>(`${environment.apiRoot}/courses`);
+    const courses$ = this.httpClient.get<GetCoursesResponse>(`${environment.apiRoot}/courses`, {
+      context: new HttpContext().set(SkipLoadingContext, true)
+    });
     const response = await firstValueFrom(courses$);
     return response.courses;
   }
--- angular-signals-course-1-start/src/app/services/loading.interceptor.ts
+++ angular-signals-course-1-start/src/app/services/loading.interceptor.ts
@@ -2,8 +2,13 @@ import { HttpHandler, HttpHandlerFn, HttpInterceptor, HttpInterceptorFn, HttpReq
 import { LoadingService } from "../loading/loading.service";
 import { inject } from "@angular/core";
 import { finalize } from "rxjs";
+import { SkipLoadingContext } from "../skip-loading/skip-loading.context";
 
 export const loadingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  if(req.context.get(SkipLoadingContext)) {
+    return next(req);
+  }
+
   const loadingService = inject(LoadingService);
 
   loadingService.loadingOn();
