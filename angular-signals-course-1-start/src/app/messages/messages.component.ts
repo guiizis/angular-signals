27575eddb1b60572d76510d65e2df2408ea839c7
guiziis <guigamoda@hotmail.com>
2025-12-07 09:48:22 -0300

    implements message component

--- angular-signals-course-1-start/src/app/messages/messages.component.ts
+++ angular-signals-course-1-start/src/app/messages/messages.component.ts
@@ -1,16 +1,21 @@
-import {Component, inject} from "@angular/core";
-import {MessagesService} from "./messages.service";
-import {NgClass} from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { MessagesService } from "./messages.service";
+import { NgClass } from "@angular/common";
 
 @Component({
-    selector: 'messages',
-    templateUrl: './messages.component.html',
-    styleUrls: ['./messages.component.scss'],
-    imports: [
-        NgClass
-    ]
+  selector: 'messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+  imports: [
+    NgClass
+  ]
 })
 export class MessagesComponent {
+  private readonly messagesService = inject(MessagesService);
+  public readonly message = this.messagesService.message;
 
+  OnClose() {
+    this.messagesService.clear();
+  }
 
 }
